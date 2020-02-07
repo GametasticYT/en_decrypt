6d1d5601e209cd43617fb497f2ffdde1fdf5287e
Sebastian Rüth <sebiruet@gmail.com>
2020-02-07 18:42:35 +0100

    initial commit

---
+++ en_decrypt.py
@@ -0,0 +1,31 @@
+import pyAesCrypt
+import os
+bufferSize = 64 * 1024
+
+
+def encrypt():
+    inputFile = str(input("Bitte Pfad der datei eingeben oder (wenn im selben ordner) dateinamen angeben: "))
+    password = str(input("Bitte Gewünschtes Passwort eingeben: "))
+    pyAesCrypt.encryptFile(inputFile, inputFile+".aes", password, bufferSize)
+    os.remove(inputFile)
+
+def decrypt():
+    inputFile = str(input("Bitte Pfad der verschlüsselten Datei oder (wenn im selben ordner) dateinamen angeben: "))
+    if inputFile.endswith('.aes'):
+        outPutfile = inputFile[:-4]
+        password = str(input("Passwort: "))
+        pyAesCrypt.decryptFile(inputFile, outPutfile, password, bufferSize)
+        os.remove(inputFile)
+    else:
+        print("Die eingegebene Datei ist nicht verschlüsselt oder existiert nicht!")
+
+
+print("1. Verschlüsseln")
+print("2. Entschlüsseln")
+choice = int(input("Bitte Funktion Wählen: "))
+if choice == 1:
+    encrypt()
+elif choice == 2:
+    decrypt()
+else:
+    print("Nicht unterstütze Wahl, bitte erneut versuchen")
